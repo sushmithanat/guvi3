ae08ea66d40c87228377f65cf11a49e8f2730276
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-31 16:45:37 +0530

    Create summax.java

---
+++ summax.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class summax
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<n;i++)
+		sum+=a[i];
+		System.out.print(sum);
+	}
+}
