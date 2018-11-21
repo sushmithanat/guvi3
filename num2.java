d3c2345b55558772a0cce3304941bcb2c864801b
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-11-21 17:56:10 +0530

    Create num2.java

---
+++ num2.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class num2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=2,cnt=0,rem=0;
+		for(int i=1;i<=n;i++)
+		{
+				n1=i;
+				while(n1>0)
+				{
+					rem = n1%10;
+					if(rem==2)
+					cnt++;
+					n1 /= 10;
+				}
+		}
+		System.out.print(cnt);
+	}
+}
