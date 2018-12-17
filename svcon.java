4f636f891f50dd70d43e711db3f226058010a033
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-12-17 16:09:13 +0530

    Create svcon.java

---
+++ svcon.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class svcon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		if(str.contains("Vishal") && str.contains("Sundar"))
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
+}
